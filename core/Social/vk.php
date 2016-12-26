a01829d039d6827a569ab21c8106021386fd72bb
Alexander <andreev.myskype@gmail.com>
2016-12-26 02:50:37 +0200

    add TODO

--- core/Social/vk.php
+++ core/Social/vk.php
@@ -1,3 +1,2 @@
 <?php
-// TODO реализовать классы для работы с API 
-// vk, facebook, twitter, google maps (помоему там js), нова пошта
+// TODO реализовать классы для работы с API vk, facebook, twitter, google maps (помоему там js), нова пошта (при помощи паттерна Адаптер)
--- frontend/web/index.php
+++ frontend/web/index.php
@@ -1,4 +1,5 @@
 <?php
+// TODO адаптировать проект под PHP7
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 

1148b3e0d6307026f53bfe726e6009eb6661e642
andreev_time <andreev.time@gmail.com>
2017-01-22 19:01:29 +0000

    Authentication

--- core/Traits/Authentication.php
+++ core/Traits/Authentication.php
@@ -1,2 +1,13 @@
 <?php
 // TODO реализовать методы аутентификации signup(), login(), logout() при помощи сессии или куков
+
+namespace core\Traits;
+
+trait Authentication
+{
+    public function signup($login, $password)
+    {
+        
+        return true;
+    }
+}
--- core/User/User.php
+++ core/User/User.php
@@ -1,2 +1,16 @@
 <?php
 // TODO реализовать модель пользователя для методов аутентификации
+namespace core\User;
+
+use core\models\Model;
+use core\Traits\Authentication;
+
+class User extends Model implements AuthenticationInterface
+{
+    use Authentication;
+    
+    public function checkPassword($password, $hashPassword)
+    {
+        return $hashPassword === md5($password);
+    }
+}

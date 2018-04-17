4be0e8f697190f9001fd044c3697f0f01b56e8c2
Alex <alex@gmail.com>
2018-04-17 09:23:18 +0300

    delete log

--- core/Iterator/Iterator.php
+++ core/Iterator/Iterator.php
@@ -13,31 +13,26 @@ class Iterator implements \Iterator
 
     public function next()
     {
-        var_dump('fun:next');
         return next($this->_items);
     }
 
     public function valid()
     {
-        var_dump('fun:valid');
         return isset($this->_items[$this->key()]);
     }
 
     public function current()
     {
-        var_dump('fun:current');
         return current($this->_items);
     }
 
     public function key()
     {
-        var_dump('fun:key');
         return key($this->_items);
     }
 
     public function rewind()
     {
-        var_dump('fun:rewind');
         return reset($this->_items);
     }
 }
